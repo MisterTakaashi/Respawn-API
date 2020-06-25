7d4b0f4d35915294f5aa8bafc8fe99cda79372f0
MisterTakaashi <soapmctravich@gmail.com>
2020-06-25 20:57:47 +0200

    Updated dependencies

--- src/server.js
+++ src/server.js
@@ -3,9 +3,12 @@ const koaJson = require('koa-json');
 
 const router = require('./router');
 
+const port = 3000;
 const app = new koa();
 
 app.use(koaJson());
 app.use(router.routes());
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log('Server listening on port', port);
+});
